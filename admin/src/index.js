f9ddd426243b6b55ac688f7c2ade08549558a48a
Misha Afanasiuk <mike.afanasiuk.21@gmail.com>
2022-01-30 23:50:25 +0100

    Added report endpoint Updated old endpoints with new services

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,22 +1,39 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("config")
-const request = require("request")
+
+const createReport = require('./services/reportService');
+const investmentService = require("./services/investmentService");
 
 const app = express()
 
-app.use(bodyParser.json({limit: "10mb"}))
+app.use(bodyParser.json({ limit: "10mb" }))
 
-app.get("/investments/:id", (req, res) => {
-  const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
-    }
-  })
+app.get("/investments/:id", async (req, res) => {
+  const { id } = req.params
+
+  try {
+    res.send(await investmentService.getById(id))
+  } catch (e) {
+    console.error(e)
+    res.sendStatus(500);
+  }
+})
+
+app.get('/report', async (req, res) => {
+  try {
+    // Basicly we can process investments response with streams, but I did not implement it here due to lack of tim
+    const investments = await investmentService.get();
+
+    const csv = await createReport(investments);
+
+    await investmentService.sendReport(csv);
+
+    res.send(csv)
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 })
 
 app.listen(config.port, (err) => {
